30eb1f7e108a485ad160825b2e41751154302f5a
y2k <itwisterlx@gmail.com>
2015-08-10 10:26:10 +0400

    init commit

---
+++ ImageKeyboard/KeyboardService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Android.App;
+using Android.InputMethodServices;
+using Android.Views;
+
+namespace ImageKeyboard
+{
+    [Service(
+        Exported = true,
+        Label = "@string/app_name",
+        Permission = "android.permission.BIND_INPUT_METHOD")]
+    [MetaData("android.view.im", Resource = "@xml/method")]
+    [IntentFilter(new[] { "android.view.InputMethod" })]
+    public class KeyboardService : InputMethodService
+    {
+        public override View OnCreateInputView()
+        {
+            var view = LayoutInflater.Inflate(Resource.Layout.keyboard, null);
+            return view;
+        }
+    }
+}
